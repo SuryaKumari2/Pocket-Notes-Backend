0d0b817eae3eb6a765ac75ed154602f86a3d2794
SuryaKumari2 <suryakumarinaidu222@gmail.com>
2024-08-09 09:26:58 +0530

    little changes

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
-const express=require('express');
-const dotenv=require('dotenv');
-const mongoose=require('mongoose');
-const bodyparser=require('body-parser');
-const cors=require('cors');
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+const cors = require('cors');
 
 
-const userRoutes=require('./routes/userRoutes')
-const groupRoutes=require('./routes/groupRoutes')
-const notesRoutes=require('./routes/notesRoutes')
+const userRoutes = require('./routes/userRoutes')
+const groupRoutes = require('./routes/groupRoutes')
+const notesRoutes = require('./routes/notesRoutes')
 dotenv.config();
 
 
 mongoose.connect(process.env.MONGO_URI)
-.then(()=>console.log('Database connected Succesfully'))
-.catch((error)=>console.log(error));
-const app=express();
-const port=process.env.PORT || 8000;
+    .then(() => console.log('Database connected Succesfully'))
+    .catch((error) => console.log(error));
+const app = express();
+const port = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(bodyparser.json());
-app.use('/api/user',userRoutes)
-app.use('/api/group',groupRoutes)
-app.use('/api/notes',notesRoutes)
-app.listen(port,()=>{
+app.use('/api/user', userRoutes)
+app.use('/api/group', groupRoutes)
+app.use('/api/notes', notesRoutes)
+app.listen(port, () => {
     console.group('server running on the port')
+})
+
+app.use('/', (req, res) => {
+    res.send('<h1>Welcome to Pocket-notes')
 })
